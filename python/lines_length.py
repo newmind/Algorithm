325b11e2de97ed24fd1cc74e73a0238d4325668a
jgkim <junggonkim@gmail.com>
2019-05-21 00:14:58 +0900

    a, b = map(int, sys.stdin.readline().rstrip().split())     lines.append((a, b))

--- python/lines_length.py
+++ python/lines_length.py
@@ -20,6 +20,21 @@ def line_sweeping_list(lines : list):
 
 
 ######
+# import sys
+# input=sys.stdin.readline
+# p=[]
+# N=int(input())
+# for _ in range(N):
+# 	a,b=map(int,input().split())
+# 	if a>b:a,b=b,a
+# 	p.append((a,b))
+# p.sort();L=0;s,e=p[0]
+# for i in range(1,N):
+# 	n,m=p[i]
+# 	if e<n:L+=e-s;s,e=n,m
+# 	elif e<m:e=m
+# print(L+e-s)
+
 
 if __name__ == "__main__":
 #   n = int(sys.stdin.readline())
@@ -32,7 +47,14 @@ if __name__ == "__main__":
 #     lines[i][0] = int(a)
 #     lines[i][1] = int(b)
 
-  import random
-  lines = [[random.randint(0, 1000), random.randint(1001, 2000)] for _ in range(1000000)]
+  # 위의 미리 할당보다, 라인읽어서 바로 split map 이 빠름. append도 빠르네
+  n = int(sys.stdin.readline())
+  lines = []
+  for i in range(n):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    lines.append((a, b))
+
+  # import random
+  # lines = [[random.randint(0, 1000), random.randint(1001, 2000)] for _ in range(1000000)]
 
   line_sweeping_list(lines)
