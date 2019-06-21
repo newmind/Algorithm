2f178c131a3685aa3658b6c19f663dc4d2ca89b3
jgkim <junggonkim@gmail.com>
2019-06-22 05:07:28 +0900

    조세퍼스 문제 0

---
+++ cpp/p11866_joseph.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+
+/**
+ * 조세퍼스 문제 0
+ * boj.kr/11866
+ */
+using namespace std;
+int main() {
+  // cin.tie(NULL);
+  ios::sync_with_stdio(false);
+
+  int N, K;
+  cin >> N >> K;
+
+  queue<int> q;
+  for (size_t i = 1; i <= N; i++) {
+    q.push(i);
+  }
+
+  cout << '<';
+
+  int count = 0;
+  while (!q.empty()) {
+    auto e = q.front();
+    q.pop();
+    count++;
+    count %= K;
+    if (count != 0) {
+      q.push(e);
+    } else {
+      cout << e << (!q.empty() ? ", " : "");
+    }
+
+  }
+  cout << '>';
+  cout.flush();
+  return 0;
+}
