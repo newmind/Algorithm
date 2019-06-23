5914f4cc22b8b6facc98e6da8c67e968c99e2b74
jgkim <junggonkim@gmail.com>
2019-06-23 22:30:19 +0900

    최대공약수를 '1' 로 출력하기

---
+++ cpp/p1850_gcd.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+/**
+ * 최소공배수
+ * boj.kr/1850
+ */
+
+typedef int64_t ll;
+
+using namespace std;
+
+#define FASTIO        \
+  std::cin.tie(NULL); \
+  std::ios::sync_with_stdio(false);
+
+ll gcd(ll a, ll b) {
+  while (b != 0) {
+    ll m = a % b;
+    a = b;
+    b = m;
+  }
+  return a;
+}
+
+int main() {
+  FASTIO;
+
+  ll a, b;
+  cin >> a >> b;
+  ll g = gcd(a, b);
+  while (g--) {
+    cout << '1';
+  }
+
+  cout.flush();
+  return 0;
+}
